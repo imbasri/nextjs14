50b7ce13cd3adfb4258715b2c0efded9e6df28c3
imbasri <imbasri.dev@gmail.com>
2025-01-02 20:04:40 -0500

    10 | Implementasi Fitur CRUD Read Data dengan MongoDB

--- app/(dashboard)/dashboard/category/page.jsx
+++ app/(dashboard)/dashboard/category/page.jsx
@@ -1,11 +1,44 @@
+import connectDB from "@/config/database";
+import Categories from "@/models/category";
 import Link from "next/link";
 import React from "react";
 
-const CategoryPage = () => {
+const CategoryPage = async () => {
+  await connectDB();
+  const categories = await Categories.find();
   return (
-    <Link className='btn btn-primary btn-sm' href={"/dashboard/category/create"}>
-      Create
-    </Link>
+    <>
+      <Link className='btn btn-primary btn-sm' href={"/dashboard/category/create"}>
+        Create
+      </Link>
+
+      {/* show table */}
+      <div className='overflow-x-auto'>
+        <table className='table table-zebra'>
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {categories.map((item, index) => (
+              <tr className='hover' key={item._id}>
+                <th>{index + 1}</th>
+                <td>{item.name}</td>
+                <td>
+                  <Link className='btn btn-sm btn-info' href={`/dashboard/category/${item._id}/edit`}>
+                    Edit
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 };
 
